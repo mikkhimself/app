e98f051fdd7925708feb0df775907783f0220309
kuttim <kuttim.mikk@gmail.com>
2020-05-04 15:34:11 +0300

    [minor]: Initial commit to the repository

---
+++ App.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { AppLoading } from 'expo';
+import { Asset } from 'expo-asset';
+import * as Font from 'expo-font';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
